0c6e8597d152ab7dbe538074c5277d6afa710aac
kmdiogo <kmdiogo@student.ysu.edu>
2019-04-02 02:30:50 -0400

    Initial Commit

---
+++ ScreepPackOutput/main.js
@@ -0,0 +1,34 @@
+var roleHarvester = require('Role.Harvester');
+var roleUpgrader = require('Role.Upgrader');
+var roleBuilder = require('Role.Builder');
+
+module.exports.loop = function () {
+
+    var tower = Game.getObjectById('TOWER_ID');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+}
---
+++ src/Utils/Examples.js
@@ -0,0 +1,7 @@
+// The root of your scripts directory (in this example 'src') can be referenced 
+// using a leading / or no leading slashs
+var example1 = require('/Role/Builder');
+var example2 = require('Role/Harvester');
+
+// Relative Paths may also be used
+var example3 = require('../Role/Upgrader');
